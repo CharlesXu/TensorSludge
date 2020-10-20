d4977cf96a3b2061d5d1506150e7032f3ddc5af2
Masterchef365 <duncan.freeman1@gmail.com>
2020-10-20 00:24:29 -0700

    Add sigmoid shader and pipeline

---
+++ shaders/compile.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+for x in *.comp; do
+    glslc -O $x -o $x.spv
+done
--- src/sigmoid.rs
+++ src/sigmoid.rs
@@ -1,8 +1,10 @@
-use anyhow::Result;
-use erupt::{vk1_0 as vk, DeviceLoader};
+use anyhow::{Result, Context};
+use erupt::{vk1_0 as vk, DeviceLoader, utils::decode_spv};
 use std::sync::Arc;
+use std::ffi::CString;
 
 pub struct Sigmoid {
+    pipeline: vk::Pipeline,
     descriptor_set_layout: vk::DescriptorSetLayout,
     device: Arc<DeviceLoader>,
 }
@@ -21,7 +23,39 @@ impl Sigmoid {
         let descriptor_set_layout =
             unsafe { device.create_descriptor_set_layout(&create_info, None, None) }.result()?;
 
+        // Load shader
+        let shader_spirv =
+            std::fs::read("shaders/sigmoid.comp.spv").context("Sigmoid shader failed to load")?;
+        let shader_decoded = decode_spv(&shader_spirv).context("Shader decode failed")?;
+        let create_info = vk::ShaderModuleCreateInfoBuilder::new().code(&shader_decoded);
+        let shader_module =
+            unsafe { device.create_shader_module(&create_info, None, None) }.result()?;
+
+        // Pipeline
+        let descriptor_set_layouts = [descriptor_set_layout];
+        let create_info =
+            vk::PipelineLayoutCreateInfoBuilder::new().set_layouts(&descriptor_set_layouts);
+        let pipeline_layout =
+            unsafe { device.create_pipeline_layout(&create_info, None, None) }.result()?;
+
+        let entry_point = CString::new("main")?;
+        let stage = vk::PipelineShaderStageCreateInfoBuilder::new()
+            .stage(vk::ShaderStageFlagBits::COMPUTE)
+            .module(shader_module)
+            .name(&entry_point)
+            .build();
+        let create_info = vk::ComputePipelineCreateInfoBuilder::new()
+            .stage(stage)
+            .layout(pipeline_layout);
+        let pipeline =
+            unsafe { device.create_compute_pipelines(None, &[create_info], None) }.result()?[0];
+
+        unsafe {
+            device.destroy_shader_module(Some(shader_module), None);
+        }
+
         Ok(Self {
+            pipeline,
             descriptor_set_layout,
             device,
         })
@@ -31,6 +65,7 @@ impl Sigmoid {
 impl Drop for Sigmoid {
     fn drop(&mut self) {
         unsafe {
+            self.device.destroy_pipeline(Some(self.pipeline), None);
             self.device
                 .destroy_descriptor_set_layout(Some(self.descriptor_set_layout), None);
         }
